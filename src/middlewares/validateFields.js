e953082fe81cd63a72671088c44d236369f52ba1
Francisco Huapaya <franciscojho@hotmail.com>
2022-03-10 15:32:42 -0500

    Remove console.log

--- src/middlewares/validateFields.js
+++ src/middlewares/validateFields.js
@@ -3,7 +3,6 @@ import { validationResult } from 'express-validator'
 const validateFields = (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        console.log(errors.errors[0])
         return res.status(400).json({ errors: errors.errors[0] })
     }
     next()
